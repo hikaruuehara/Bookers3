eea3e1c90825143c0e0c7cb6d06e6077a89429bc
EC2 Default User <ec2-user@ip-172-31-18-49.ec2.internal>
2020-10-13 06:01:15 +0000

    [finish]Bookers

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '25fdb7c8bf5d9f599ec58d08916a989b5d5afbdcf7b8258e49289409184f2c878def0188de2e576ea515a268f07bf40464309f3dce3b8fddc7c0223eb89f8a2f'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#top'
+  get "home/about" =>"homes#about"
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update, :create, :index, :new]
+  resources :books, only: [:create, :index, :show, :update, :edit, :destroy ]
 end
